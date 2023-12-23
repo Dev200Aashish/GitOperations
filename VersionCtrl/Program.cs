b52120195fd3dbd26bfedfbcf06369f04a4fe2a3
Dev200Aashish <devashu0016@gmail.com>
2023-12-23 10:22:19 +0530

    adding file

---
+++ VersionCtrl/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using VersionCtrl;
+
+public class Start
+{
+    public static void Main(string [] args)
+    {
+        MyDetails myDetails = new MyDetails();
+        myDetails.details();
+    }
+}
+
