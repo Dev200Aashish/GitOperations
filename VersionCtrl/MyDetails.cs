0856fc08dffe9a7a9611fef312cfd15287e9fb9f
Dev200Aashish <devashu0016@gmail.com>
2023-12-23 10:56:33 +0530

    Commit ti Phase1

--- VersionCtrl/MyDetails.cs
+++ VersionCtrl/MyDetails.cs
@@ -13,7 +13,7 @@ namespace VersionCtrl
             Console.WriteLine("Edited From VS");
             Console.WriteLine("Aashish");
             
-            Console.WriteLine("Phase 1");
+            Console.WriteLine("Edit by me Phase 1");
         }
     }
 }
